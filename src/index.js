344d3968848337617eca2297f3d14600a42f15dd
Vinicius A N Silva <viniciusansilva@outlook.com>
2022-09-10 16:00:42 -0300

    init servidor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 //execução do servidor
+const express = require("express");
+
+const server = express();
+
+app.use(express.json());
+
+server.listen(3306, () => console.log("Servidor rodando na porta 3306"));
