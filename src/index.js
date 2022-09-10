cffecce8a38f0d8885db298a3eedcbc8f0651b88
andradean <109110750+andradean@users.noreply.github.com>
2022-09-10 17:04:38 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ const server = express();
 
 app.use(express.json());
 
-server.listen(3306, () => console.log("Servidor rodando na porta 3306"));
+server.listen(3000, () => console.log("Servidor rodando na porta 3306"));
