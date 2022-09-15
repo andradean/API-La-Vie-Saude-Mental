ec79ccb533031ed9ffbcd8df46ba9f43170738f5
Vinicius A N Silva <viniciusansilva@outlook.com>
2022-09-15 19:44:35 -0300

    login corrigido

--- src/controller/authcontroller.js
+++ src/controller/authcontroller.js
@@ -1,31 +1,28 @@
 const jwt = require("jsonwebtoken");
 const secret = require("../configs/secret");
 const bcrypt = require("bcrypt");
-const psicologo = require("../models/psicologos");
+const { psicologo } = require("../models/index");
 const login = require("../validations/auth/login");
 
 const authController = {
   async login(req, res) {
     const { email, senha } = req.body;
-    try {
-      const user = await psicologo.findOne({
-        where: { email: email },
-      });
-      if (!bcrypt.compareSync(senha, user.senha)) {
-        return res.status(401).json("senha incorreta");
-      }
-      const token = jwt.sign(
-        {
-          psicologo_id: user.id,
-          email: user.email,
-          nome: user.nome,
-        },
-        secret.key
-      );
-      return res.status(200).json(token);
-    } catch {
-      console.log(error);
+
+    const user = await psicologo.findOne({
+      where: { email: email },
+    });
+    if (!bcrypt.compareSync(senha, user.senha)) {
+      return res.status(401).json("senha incorreta");
     }
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        nome: user.nome,
+      },
+      secret.key
+    );
+    return res.status(200).json(token);
   },
 };
 
